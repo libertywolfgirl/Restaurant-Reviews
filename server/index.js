7f3a7b2bcb26e8aa67ce4c2621c5476444de3577
Glitch (starter-cra-and-express) <none>
2021-05-29 22:38:12 +0000

    👑🌠 Checkpoint ./server/index.js:16231546/742

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
-const app = require('./server.js');
-const mongodb = require('mongodb');
-const dotenv = require('dotenv');
+const app = require("./server.js");
+const mongodb = require("mongodb");
+const dotenv = require("dotenv");
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect(
-  process.env.RESTREVIEWS_DB_URI,
-  {
-    poolSize: 50,
-    wtimeout: 2500,
-    useNewUrlParse: true
-  }
-).catch(err => {
-  console.error
+MongoClient.connect(process.env.RESTREVIEWS_DB_URI, {
+  poolSize: 50,
+  wtimeout: 2500,
+  useNewUrlParse: true
+}).catch(err => {
+  console.error(err.stack);
+  process.exit(1);
 })
