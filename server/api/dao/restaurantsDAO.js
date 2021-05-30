5ef42f7313d1745a673ca991d51fbdadf3892edc
Glitch (starter-cra-and-express) <none>
2021-05-30 00:54:56 +0000

    👖🛥 Checkpoint ./server/api/dao/restaurantsDAO.js:16231546/4584

--- server/api/dao/restaurantsDAO.js
+++ server/api/dao/restaurantsDAO.js
@@ -15,6 +15,32 @@ class RestaurantDAO {
       );
     }
   }
+
+  static async getRestaurants({
+    filters = null,
+    page = 0,
+    restaurantsPerPage = 20
+  } = {}) {
+    let query;
+    if (filters) {
+      if ("name" in filters) {
+        query = { $text: { $search: filters["name"] } };
+      } else if ("cuisine" in filters) {
+        query = { cuisine: { $eq: filters["cuisine"] } };
+      } else if ("zipcode" in filters) {
+        query = { "address.zipcode": { $eq: filters["zipcode"] } };
+      }
+    }
+
+    let cursor;
+
+    try {
+      cursor = await restaurants.find(query);
+    } catch (e) {
+      console.error(`Unable to issue find command, ${e}`);
+      return { restaurantsList: [], totalNumRestaurants: 0 };
+    }
+  }
 }
 
 module.exports = RestaurantDAO;
