58ea8955bf30e2e5aabe62e26672c227dfb0b8d6
Glitch (starter-cra-and-express) <none>
2021-06-05 23:33:14 +0000

    📅🚘 Checkpoint ./server/api/restaurants.route.js:16231546/231

--- server/api/restaurants.route.js
+++ server/api/restaurants.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const RestaurantsCtrl = require('./restaurants.controller.js')
+const ReviewsCtrl = require('./reviews.controller.js')
 
 const router = express.Router();
 
 router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
+
+router.route('/review').post(ReviewsCtrl.apiPostReview).put(ReviewsCtrl.apiUpdateReview).delte(ReviewsCtrl.apiDeleteReview);
 
 module.exports = router;
