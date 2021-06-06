5e97a842894c5f02509713ed3abae95e8739d58b
Glitch (starter-cra-and-express) <none>
2021-06-06 02:34:33 +0000

    🏫🌏 Checkpoint ./package.json:16231546/4635 ./src/App.js:16231546/575 ./src/components/add-review.js:16231546/370 ./src/components/login.js:16231546/180 ./src/components/restaurants-list.js:16231546/200 ./src/components/restaurants.js:16231546/192

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and your changes will live-update automatically.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World.
     </div>
   );
 }
